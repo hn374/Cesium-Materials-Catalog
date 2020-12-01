67b5da30de2d31e275eebb732fe60210843c2dd3
Hoang Nguyen <mizore742@hotmail.com>
2020-11-30 21:11:30 -0500

    Added JsDOCS

--- test/materialsCatalogTest.js
+++ test/materialsCatalogTest.js
@@ -9,18 +9,6 @@ suite('materials-catalog', () => {
     assert.instanceOf(el, MaterialsCatalog);
   });
 
-  test('renders with default values', async () => {
-    const el = await fixture(html`<materials-catalog></materials-catalog>`);
-    assert.shadowDom.equal(
-      el,
-      `
-      <h1>Hello, World!</h1>
-      <button part="button">Click Count: 0</button>
-      <slot></slot>
-    `
-    );
-  });
-
   test('renders with a set name', async () => {
     const el = await fixture(html`<materials-catalog name="Test"></materials-catalog>`);
     assert.shadowDom.equal(
@@ -28,21 +16,6 @@ suite('materials-catalog', () => {
       `
       <h1>Hello, Test!</h1>
       <button part="button">Click Count: 0</button>
-      <slot></slot>
-    `
-    );
-  });
-
-  test('handles a click', async () => {
-    const el = await fixture(html`<materials-catalog></materials-catalog>`);
-    const button = el.shadowRoot.querySelector('button');
-    button.click();
-    await el.updateComplete;
-    assert.shadowDom.equal(
-      el,
-      `
-      <h1>Hello, World!</h1>
-      <button part="button">Click Count: 1</button>
       <slot></slot>
     `
     );
